16c5e73625bae7c111c16d860d3f0ce61c4db658
dminikulin <dminikulin2003@gmail.com>
2025-05-01 09:30:01 +0300

    Optimizations inside the Converter component

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,68 +1,64 @@
 <script setup>
 import { ref, computed, watch } from 'vue';
 
-const number_to_convert = ref('');
+const inputNumber = ref('');
 const result = ref(null);
-const convert_from = ref('');
-const convert_to = ref('');
+const convertFrom = ref('');
+const convertTo = ref('');
+const convertedFrom = ref('');
+const convertedTo = ref('');
+const convertedInput = ref('');
 
-const optionsChosen = computed(() => {
-    return convert_from.value === '' || convert_to.value === '';
+const SYSTEMS = {
+  'Decimal': 10,
+  'Binary': 2,
+  'Hexadecimal': 16,
+  'Octal': 8
+};
+
+const areOptionsMissing = computed(() => {
+    return convertFrom.value === '' || convertTo.value === '';
 })
 
 const isNumberValid = computed(() => {
-    switch (convert_from.value) {
+    switch (convertFrom.value) {
         case 'Decimal':
-            return number_to_convert.value === '' || /^[+]?\d+$/.test(number_to_convert.value);
+            return inputNumber.value === '' || /^[+-]?\d+$/.test(inputNumber.value);
         case 'Binary':
-            return number_to_convert.value === '' || /^[01]+$/.test(number_to_convert.value);
+            return inputNumber.value === '' || /^[01]+$/.test(inputNumber.value);
         case 'Hexadecimal':
-            return number_to_convert.value === '' || /[0-9A-Fa-f]+$/.test(number_to_convert.value);
+            return inputNumber.value === '' || /^[0-9A-Fa-f]+$/.test(inputNumber.value);
         case 'Octal':
-            return number_to_convert.value === '' || /^[0-7]+$/.test(number_to_convert.value)
+            return inputNumber.value === '' || /^[0-7]+$/.test(inputNumber.value)
     }
 })
 
 const sameSystem = computed(() => {
-    return convert_from.value === convert_to.value && (convert_from.value !== '' && convert_to.value !== '');
+    return convertFrom.value === convertTo.value && (convertFrom.value !== '' && convertTo.value !== '');
 })
 
 function swap() {
-    let temp = convert_from.value;
-    convert_from.value = convert_to.value;
-    convert_to.value = temp;
+    let temp = convertFrom.value;
+    convertFrom.value = convertTo.value;
+    convertTo.value = temp;
 }
 
 function conversion() {
-    let base = 0;
-    switch (convert_from.value) {
-        case 'Decimal': base = 10; break;
-        case 'Binary': base = 2; break;
-        case 'Hexadecimal': base = 16; break;
-        case 'Octal': base = 8; break;
-    }
+    if (!isNumberValid.value || areOptionsMissing.value || sameSystem.value) return;
 
-    let dec = parseInt(number_to_convert.value, base);
+    const baseFrom = SYSTEMS[convertFrom.value]
+    const baseTo = SYSTEMS[convertTo.value];
 
-    switch (convert_to.value) {
-        case 'Decimal':
-            result.value = dec.toString(10);
-            break;
-        case 'Binary':
-            result.value = dec.toString(2);
-            break;
-        case 'Hexadecimal':
-            result.value = dec.toString(16).toUpperCase();
-            break;
-        case 'Octal':
-            result.value = dec.toString(8);
-            break;
-    }
+    const dec = parseInt(inputNumber.value, baseFrom);
+
+    convertedInput.value = inputNumber.value;
+    convertedFrom.value = convertFrom.value;
+    convertedTo.value = convertTo.value;
+
+    result.value = convertTo.value === 'Hexadecimal'
+        ? dec.toString(baseTo).toUpperCase()
+        : dec.toString(baseTo);
 }
-
-watch([number_to_convert, convert_to, convert_from], () => {
-  result.value = null;
-});
 
 </script>
 
@@ -70,26 +66,20 @@ watch([number_to_convert, convert_to, convert_from], () => {
     <div class="col-12 col-sm-12 col-md-10 col-lg-8 col-xl-6">    
         <div class="row g-2 my-2">
             <div class="col-12 col-md-5">
-                <select v-model="convert_from" class="form-select" :class="{'is-invalid': sameSystem}" placeholder="Convert from:">
+                <select v-model="convertFrom"  class="form-select" :class="{'is-invalid': sameSystem}" placeholder="Convert from:">
                     <option disabled value="">Choose numeric system:</option>
-                    <option>Decimal</option>
-                    <option>Binary</option>
-                    <option>Hexadecimal</option>
-                    <option>Octal</option>
+                    <option v-for="obj in Object.keys(SYSTEMS)" :key="obj">{{obj}}</option>
                 </select>
             </div>
             <div class="col-12 col-md-2 d-flex justify-content-center align-items-md-center">
-                <button @click="swap" class="btn border-0" :disabled="optionsChosen || sameSystem">
+                <button @click="swap" class="btn border-0" :disabled="areOptionsMissing || sameSystem">
                     <img src="/src/assets/arrow-left-right.svg" alt="swap" width="20" height="20">
                 </button>
             </div>
             <div class="col-12 col-md-5">
-                <select v-model="convert_to" class="form-select" :class="{'is-invalid': sameSystem}" placeholder="Convert to:">
+                <select v-model="convertTo" class="form-select" :class="{'is-invalid': sameSystem}" placeholder="Convert to:">
                     <option disabled value="">Choose numeric system:</option>
-                    <option>Decimal</option>
-                    <option>Binary</option>
-                    <option>Hexadecimal</option>
-                    <option>Octal</option>
+                    <option v-for="obj in Object.keys(SYSTEMS)" :key="obj">{{obj}}</option>
                 </select>
             </div>
             <div class="col-12">
@@ -97,14 +87,14 @@ watch([number_to_convert, convert_to, convert_from], () => {
             </div>
         </div>
         <div class="my-2">
-            <input v-model="number_to_convert" :disabled="optionsChosen || sameSystem" class="form-control" :class="{'is-invalid': !isNumberValid && !optionsChosen}" type="text" placeholder="Enter number...">
-            <p v-if="!isNumberValid && !optionsChosen" class="invalid-feedback">Number does not exist in {{convert_from.toLowerCase()}} system.</p>
+            <input v-model="inputNumber" :disabled="areOptionsMissing || sameSystem" class="form-control" :class="{'is-invalid': !isNumberValid && !areOptionsMissing}" type="text" placeholder="Enter number...">
+            <p v-if="!isNumberValid && !areOptionsMissing" class="invalid-feedback">Number does not exist in {{convertFrom.toLowerCase()}} system.</p>
         </div>
         <div class="my-2">
-            <button @click="conversion" class="btn btn-dark w-100">Convert</button>
+            <button @click="conversion" class="btn btn-dark w-100" :disabled="!isNumberValid || areOptionsMissing || sameSystem">Convert</button>
         </div>
         <div v-if="result !== null" class="my-3">
-            <h5 class="text-xl text-center text-break">{{convert_from}} <span class="fw-bold">{{ number_to_convert }}</span> to {{ convert_to.toLowerCase() }} is: <span class="fw-bold">{{ result }}</span></h5>
+            <h5 class="text-xl text-center text-break">{{convertedFrom}} <span class="fw-bold">{{ convertedInput }}</span> to {{ convertedTo.toLowerCase() }} is: <span class="fw-bold">{{ result }}</span></h5>
         </div>
     </div>
 </template>
